3b29f9439e1a9992ff7082d3dc4a1cf913afce9c
Stan <stan.naspinski@sustainment.tech>
2023-09-24 18:13:38 -0500

    updated tests and libs

--- Naspinski.Messaging.Tests/Keys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Naspinski.Messaging.Tests
-{
-    public static partial class Keys
-    {
-        public static readonly string SendgridApiKey = "#{SENDGRID_API_KEY}#";
-        public static readonly string FromEmail = "#{FROM_EMAIL}#";
-        public static readonly string ToEmail = "#{TO_EMAIL}#";
-    }
-}
